f2353ddbd8fae991d68f8ebaea27dfdfb5701dfd
unknown <ludoalta@gmail.com>
2017-10-05 15:02:37 +0200

    initial commit

---
+++ comment_y_aller/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace comment_y_aller.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
